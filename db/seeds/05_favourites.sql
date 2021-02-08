ffe9ab78e340dc98d72ae176ec0d6b013ac50066
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-02-08 20:45:08 +0000

    add seeds to markers and favourites

--- db/seeds/05_favourites.sql
+++ db/seeds/05_favourites.sql
@@ -1,1 +1,8 @@
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(1, 1, NULL);
 
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(2, 6, NULL);
+
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(2, 3, NULL);
