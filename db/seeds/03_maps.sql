387eb8cd27c79a008efc3e78bd2ae866677b0157
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-02-08 17:38:09 +0000

    add seeds to maps

--- db/seeds/03_maps.sql
+++ db/seeds/03_maps.sql
@@ -6,3 +6,15 @@ VALUES (2, 1, 'Art Tour', 2020-09-15 10:00:50, 2021-02-05 14:23:54)
 
 INSERT INTO maps (owner_id, interest_id, name, created_at, last_edited)
 VALUES (4, 5, 'Famous Landmarks', 2021-02-07 10:00:50, 2021-02-08 16:50:54)
+
+INSERT INTO maps (owner_id, interest_id, name, created_at, last_edited)
+VALUES (1, 7, 'yay shopping!', 2020-12-09 15:40:40, 2021-01-10 14:52:24)
+
+INSERT INTO maps (owner_id, interest_id, name, created_at, last_edited)
+VALUES (3, 7, 'Shopping Destinations', 2020-08-19 14:40:40, 2020-12-01 10:20:24)
+
+INSERT INTO maps (owner_id, interest_id, name, created_at, last_edited)
+VALUES (1, 8, 'I Like Sports', 2021-01-19 06:40:40, 2021-01-20 07:43:24)
+
+
+
