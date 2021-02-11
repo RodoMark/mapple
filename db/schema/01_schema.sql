74d0180983151fd18dab7f5375b2ad31a8ffdae9
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-02-11 14:11:42 +0000

    copy migrations into schema, using schema from now on

---
+++ db/schema/01_schema.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS interests CASCADE;
+DROP TABLE IF EXISTS maps CASCADE;
+DROP TABLE IF EXISTS markers CASCADE;
+DROP TABLE IF EXISTS favourites CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) DEFAULT NULL
+);
+
+CREATE TABLE interests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) NOT NULL,
+  interest_id INTEGER REFERENCES interests(id) NOT NULL,
+  name VARCHAR(32) DEFAULT 'My Map',
+  description TEXT,
+  created_at TIMESTAMP,
+  last_edited TIMESTAMP
+);
+
+
+CREATE TABLE markers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  map_id INTEGER REFERENCES maps(id),
+  lat DECIMAL(8, 6) NOT NULL,
+  lng DECIMAL(8, 6) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP
+);
+
+CREATE TABLE favourites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  map_id INTEGER REFERENCES maps(id),
+  created_at TIMESTAMP
+);
+
