0604781a02d58c6af023a141341ec3badd6a0ad2
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-02-11 04:02:49 +0000

    add jquery markerForm script

--- public/scripts/markerForm.js
+++ public/scripts/markerForm.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
   console.log("Marker Form: ready...")
 
-  const $marker = $('leaflet-popup-content').parent().find()
-  const $edit = $('.edit-btn')
+  const $edit = $('#mapid').parents().find('button.edit-btn')
+  const $marker = $('#mapid').parents().find('div.edit-btn')
 
   $edit.on('click', function(e){
     e.preventDefault()
 
     console.log("edit clicked")
-    $marker.empty()
+    $marker.toggle()
 
   })
 
