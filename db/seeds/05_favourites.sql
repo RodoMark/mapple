1e35b2f47a5e1f5a17a6c204236775c112afad4e
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-02-11 14:09:41 +0000

    reseed favourites with current_timestamp

--- db/seeds/05_favourites.sql
+++ db/seeds/05_favourites.sql
@@ -1,8 +1,20 @@
 INSERT INTO favourites (user_id, map_id, created_at)
-VALUES(1, 1, NULL);
+VALUES(1, 1, current_timestamp);
 
 INSERT INTO favourites (user_id, map_id, created_at)
-VALUES(2, 6, NULL);
+VALUES(2, 6, current_timestamp);
 
 INSERT INTO favourites (user_id, map_id, created_at)
-VALUES(2, 3, NULL);
+VALUES(2, 3, current_timestamp);
+
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(2, 2, current_timestamp);
+
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(1, 4, current_timestamp);
+
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(3, 2, current_timestamp);
+
+INSERT INTO favourites (user_id, map_id, created_at)
+VALUES(4, 1, current_timestamp);
